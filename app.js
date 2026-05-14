8ef1db331385c9128de3d04272045d9e3c11f4b1
junjuny0227 <junjuny0227@gmail.com>
2026-05-14 16:54:14 +0900

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    name: '전준연',
+    school: 'Gwangju Software Meister High School',
+    major: 'Front-End Development',
+    skills: ['TypeScript', 'Next.js'],
+    blog: 'https://velog.io/@junjuny0227',
+    email: 'junjuny.dev@gmail.com',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
